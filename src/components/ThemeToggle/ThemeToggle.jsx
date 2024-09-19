534258d95ad556d5e6fd610e8cc6d26da2c37e62
gs0025 <gurusaranm0025@gmail.com>
2024-09-19 15:51:28 +0530

    theme changes

--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -1,31 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import "./ThemeToggle.css"
 import Icon from "../icons/Icon"
 
 function ThemeToggle() {
-    const [icon, setIcon] = useState("moon")
-
-    useEffect(() => {
-        let darkMode = localStorage.getItem('dark-mode');
-        if (darkMode) {
-            document.body.setAttribute('data-theme', 'dark')
-            setIcon("sun")
-        } else {
-            document.body.setAttribute('data-theme', 'light')
-            setIcon("moon")
-        }
-    }, [])
-
+    const [icon, setIcon] = useState("sun")
+    document.body.setAttribute('data-theme', 'dark')
 
     function onThemeChange() {
         if (icon === "moon") {
             setIcon('sun')
             document.body.setAttribute('data-theme', 'dark')
-            localStorage.setItem('dark-mode', false)
         } else {
             setIcon('moon')
             document.body.setAttribute('data-theme', 'light')
-            localStorage.setItem('dark-mode', true)
         }
     }
 
